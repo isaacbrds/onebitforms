0d54eaf774a1d52cb1f0874a1f019fc5ae7e0985
ibrds <ibrds@ig.com.br>
2018-04-04 10:42:20 -0300

    Devise Configured

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+config.include Devise::Test::ControllerHelpers, type: :controller
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -60,4 +61,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
 end
